2fe7f224001445f4f92cfc02dc84ed7478609172
rohaizi mohamed <rohaizimohamed96@gmail.com>
2021-01-20 12:48:54 +0800

    Bismillah

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Selamat Datang Semua
         </h1>
 
         <p className={styles.description}>
